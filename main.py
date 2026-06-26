37cb781911564f1816fa180300e472bae95b2a02
gianpaoloambrosecchia-wq <gianpaolo.ambrosecchia@gmail.com>
2026-06-26 10:46:54 +0200

    informazioni generali su funzioni di nx

--- main.py
+++ main.py
@@ -14,3 +14,27 @@ def main(page: ft.Page):
 
 
 ft.app(target=main)
+
+
+# DFS (Depth First Search)
+#
+# nx.dfs_edges(G, source=nodo) → restituisce gli archi della visita
+# nx.dfs_tree(G, source=nodo) → restituisce il grafo albero DFS
+# list(nx.dfs_preorder_nodes(G, source=nodo)) → lista nodi in ordine di visita
+#
+# BFS (Breadth First Search)
+#
+# nx.bfs_edges(G, source=nodo) → restituisce gli archi della visita
+# nx.bfs_tree(G, source=nodo) → restituisce il grafo albero BFS
+# list(nx.bfs_layers(G, [nodo])) → nodi divisi per livello
+#
+# Cammini minimi
+#
+# nx.shortest_path(G, source=a, target=b, weight='weight') → cammino minimo
+# nx.shortest_path_length(G, source=a, target=b, weight='weight') → solo la distanza
+# nx.dijkstra_path(G, source=a, target=b, weight='weight') → equivalente con Dijkstra
+
+
+# Non diretto → nx.connected_components(G)
+# Diretto weakly → nx.weakly_connected_components(G)
+# Diretto strongly → nx.strongly_connected_components(G)
